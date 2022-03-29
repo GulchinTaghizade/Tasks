0502fcf2914bbc0916a23e8a2f27d3c1a39db77c
GulchinTaghizade <1pylr6h@code.edu.az>
2022-03-30 03:10:05 +0400

    uploaded homework

---
+++ Access Modifiers. Encapsulation/Tasks/Student.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tasks.Access_Modifiers._Encapsulation
+{
+    class Student //a:)Student classimiz var- id,ad ,soyad, ixtisas kimi propertiler
+    {
+
+        public int Id
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return Name;
+            }
+            set
+            {
+                Name = value;
+            }
+        }
+        public string Surname
+        {
+            get
+            {
+                return Surname;
+            }
+            set
+            {
+                Surname = value;
+            }
+        }
+        public string Speciality
+        {
+            get
+            {
+                return Speciality;
+            }
+            set
+            {
+                Id = Speciality;
+            }
+        }
+
+
+        public Student(int id,string name,string surname, string speciality)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+            Speciality = speciality;
+
+        }
+
+    }
+}
