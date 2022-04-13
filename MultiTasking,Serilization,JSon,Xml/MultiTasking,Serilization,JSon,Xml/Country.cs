5152dbc4601f64118e835b73031ac8a27c719ea7
GulchinTaghizade <100687270+GulchinTaghizade@users.noreply.github.com>
2022-04-13 23:39:46 +0400

    uploaded hometask

---
+++ MultiTasking,Serilization,JSon,Xml/MultiTasking,Serilization,JSon,Xml/Country.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MultiTasking_Serilization_JSon_Xml
+{
+    class Country
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Population { get; set; }
+    }
+}
